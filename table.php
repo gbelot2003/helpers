d7ce5db9574c9ca6cac9d6fe594a9604cfb12f17
Gerardo Belot <gerardobelot@outlook.com>
2013-01-12 23:29:34 -0600

    Update table.php

--- table.php
+++ table.php
@@ -1,41 +1,5 @@
 <?php
 
-class Table{
-    public static function Simple($headers, $query = NULL, $atributes = NULL){
-    Tag::css('tables/blueOne/blueOne');
-    $i = 1;
-    ?>
-    <?php if($headers == NULL || !$headers): ?>
-        <h6>No hay Datos Disponbles</h6>
-    <?php else: ?>
-    <table id="<?php print $atributes ?>">
-        <thead>
-            <tr>
-                <?php foreach($headers as $key => $value): ?>
-                    <th><?php print $value['title'] ?></th>
-                <?php endforeach ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($query as $query): ?>
-
-                <tr class="trTable <?php print $i++ ?>">
-                    <?php foreach($headers as $key => $value): ?> 
-                        <?php if(!isset($value['url'])): ?>              
-                            <td class="tdTable"><?php print $query->$key ?></td>
-                        <?php else: ?>
-                            <td class="tdTable"><?php print html::link($value['url'].$query->id, $query->$key, "class = '".$value['class']."' rel='".$query->id."'"
-                            )?>
-                            </td>
-                        <?php endif ?>
-                    <?php endforeach ?>
-                </tr>
-            <?php endforeach ?>         
-        </tbody>
-    </table>
-    <?php endif; ?>
-    <?php
-    }
 /**
  * Helper Table
  * @$headers => Array con los campos a mostrar, estos deben coincidir con la consulta 
@@ -57,10 +21,12 @@ class Table{
  * 
  *ejemp:
  *'id' => array ('title' => "registro No.", 'url' => 'registros/ver/', 'attr' => "class='edit' rel='group-1'").
- *
- * */
+ * 
+ */
 
-    public static function simple2($headers, $query = NULL, $atributes = NULL){
+class Table{
+
+    public static function simple($headers, $query = NULL, $atributes = NULL){
         Tag::css('tables/blueOne/blueOne');
         $i = 1;
         ?>
@@ -81,7 +47,7 @@ class Table{
                     <tr class="trTable <?php print $i++ ?>">
                         <?php foreach($headers as $key => $value): ?> 
                             <?php if(!isset($value['url'])): ?>              
-                                <td <?php if(isset($value['attr'])) { print $value['attr']; } ?>><span><?php print $query->$key ?></span></td>
+                                <td <?php print $value['attr']?>><span><?php print $query->$key ?></span></td>
                             <?php else: ?>
                                 <td class="tdTable"><?php print html::link($value['url'].$query->id, $query->$key, $value['attr']) ?></td>
                             <?php endif ?>
